c59e9644649f464ffcecd6edb6135a3c76d24be7
NicholasDewberryOfficial <85315604+NicholasDewberryOfficial@users.noreply.github.com>
2024-01-07 14:30:55 -0500

    updating everything

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Nicholas Dewberry\'s blog';
+export const SITE_DESCRIPTION = 'Note: Blog was created using ASTRO blog template - and by following a tutorial. Nothing complex has been done so far! Just a beginner blog, will get more complex with time !';
